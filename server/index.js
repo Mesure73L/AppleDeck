1680da0e5f264e8ffb6ebf56780731bcc933cc15
Mesure73L <115181664+Mesure73L@users.noreply.github.com>
2024-10-16 20:04:43 +0000

    Added a form + functionality to the join HTML and a token system

--- server/index.js
+++ server/index.js
@@ -12,6 +12,8 @@ const hostPrefix = "[HOST] ";
 const specPrefix = "[SPEC] ";
 const nodePrefix = "[NODE] ";
 
+const tokenLength = 3;
+
 // SETUP SERVERS
 
 const express = require("express");
@@ -38,12 +40,23 @@ const dir = __dirname.replace("server", "client");
 // OTHER SETUP
 
 let connected = [];
+let userTokens = [];
 let banned = [];
 let history = [];
 
 function out(message) {
     console.log(message);
     history.push(message);
+}
+
+function generateToken() {
+    let token = "";
+
+    for (let i = 0; i < tokenLength; i++) {
+        token += Math.random().toString(36);
+    }
+
+    return token.replaceAll("0.", "");
 }
 
 // JOIN
@@ -65,6 +78,40 @@ join.on("connect", socket => {
 
     socket.on("disconnect", () => {
         jout("client disconnected");
+    });
+
+    // ---
+
+    socket.on("New User", username => {
+        jout(username + " is requesting to join");
+
+        if (connected.includes(username)) {
+            return join.emit("User Approval", {
+                username: username,
+                permission: false,
+                reason: "Nickname is already in use!"
+            });
+        }
+
+        if (banned.includes(username)) {
+            return join.emit("User Approval", {
+                username: username,
+                permission: false,
+                reason: "Nickname is banned!"
+            });
+        }
+
+        const token = generateToken();
+        jout(username + " was approved");
+        jout(username + "'s token is " + token);
+        connected.push(username);
+        userTokens.push({username: username, token: token});
+
+        join.emit("User Approval", {
+            username: username,
+            permission: true,
+            token: token
+        });
     });
 });
 

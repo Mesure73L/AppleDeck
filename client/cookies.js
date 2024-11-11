498382a709809f5c4508fe13aa711dfa1a39d2ab
Mesure73L <115181664+Mesure73L@users.noreply.github.com>
2024-11-11 16:51:50 +0000

    Security fixes in cookie handling

--- client/cookies.js
+++ client/cookies.js
@@ -2,7 +2,9 @@ function setCookie(cname, cvalue, exdays) {
     const d = new Date();
     d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
     let expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + btoa(JSON.stringify(cvalue)) + ";" + expires + ";path=/";
+    let secure = location.protocol === 'https:' ? "Secure;" : "";
+    let sameSite = "SameSite=Strict";
+    document.cookie = cname + "=" + encodeURIComponent(btoa(JSON.stringify(cvalue))) + ";" + expires + ";path=/;" + secure + sameSite;
 }
 
 function getCookie(cname) {
@@ -14,7 +16,7 @@ function getCookie(cname) {
             c = c.substring(1);
         }
         if (c.indexOf(name) == 0) {
-            return JSON.parse(atob(c.substring(name.length, c.length)));
+            return JSON.parse(atob(decodeURIComponent(c.substring(name.length, c.length))));
         }
     }
     return null;
